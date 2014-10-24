4701514b522b99a486c813269f532dce937e4768
moe-ped <m.berning@web.de>
2014-10-24 11:50:42 +0200

    started Unity project

---
+++ Assets/EventManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+	public delegate void AITurn(Vector3 target);
+	public static event AITurn OnAITurn;
+
+	public Transform player;
+
+	bool pressedButton = false;
+	
+	void Update () {
+		if ((Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && !pressedButton) {
+			OnAITurn(player.position);
+			pressedButton = true;
+		}
+		else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0) {
+			pressedButton = false;
+		}
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	bool pressedButton = false;
+
+	void Update () {
+		if ((Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && !pressedButton) {
+			transform.position += (Vector3.right*Input.GetAxis("Horizontal")).normalized;
+			transform.position += (Vector3.up*Input.GetAxis("Vertical")).normalized;
+			pressedButton = true;
+		}
+		else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0) {
+			pressedButton = false;
+		}
+	}
+}
+
