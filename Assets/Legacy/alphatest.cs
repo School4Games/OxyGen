ba7f1f0caa1aadf2d71f2fcbf36437275b0cbed9
moe-ped <m.berning@web.de>
2014-10-31 01:38:47 +0100

    alpha brush test
    results satisfying
    continue testing ...

--- Assets/Legacy/alphatest.cs
+++ Assets/Legacy/alphatest.cs
@@ -5,16 +5,35 @@ public class alphatest : MonoBehaviour {
 
 	Texture2D main;
 
-	Color[] stamp = new Color[100];
+	public Texture2D stamp;
+	public int hexSideLength = 64;
+
+	//Color[] stamp = new Color[100];
 
 	// Use this for initialization
 	void Start () {
-		main = (Texture2D)renderer.material.mainTexture;
-		for(int i=0; i<stamp.Length; i++) {
-			stamp[i] = new Color(0,0,0,0);
+		if (stamp != null) {
+			main = (Texture2D)renderer.material.mainTexture;
+			Color[] colors = new Color[stamp.height * stamp.width];
+			Color[] oldColors = new Color[stamp.height * stamp.width];
+			/*for(int y=0; y<stamp.height; y++) {
+				for(int x=0; x<stamp.width; x++) {
+
+				}
+				//stamp[i] = new Color(0,0,0,0);
+			}*/
+			colors = stamp.GetPixels(0, 0, stamp.width, stamp.height);
+			int x = Random.Range(0, main.width-hexSideLength);
+			int y = Random.Range(0, main.width-hexSideLength);
+			oldColors = main.GetPixels(x, y, stamp.width, stamp.height);
+			string log = "";
+			for (int i=0; i<colors.Length; i++) {
+				oldColors[i].a = colors[i].grayscale; 
+			}
+			Debug.Log(log);
+			main.SetPixels(x, y, stamp.width, stamp.height, oldColors);
+			main.Apply();
 		}
-		main.SetPixels(Random.Range(0, main.width-5), Random.Range(0, main.height-5), 10, 10, stamp);
-		main.Apply();
 	}
 	
 	// Update is called once per frame
