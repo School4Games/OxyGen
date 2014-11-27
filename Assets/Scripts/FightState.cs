8dc20612b295503bd7c41d9aacbd76bef261790c
moe-ped <m.berning@web.de>
2014-11-27 01:48:19 +0100

    fighting system II
    - added actual function for distribution of probabilities
    - finished pointer that visually cycles through
    - added some more information for the player to see
    - improved betting mechanic (boosting loot/ shields shares a limit)
    - fixed bug where you could bet more water than you had (makes sliders
    really irritating, though ^^)

--- Assets/Scripts/FightState.cs
+++ Assets/Scripts/FightState.cs
@@ -20,6 +20,8 @@ public class FightState : MonoBehaviour
 	int dmgX = 0;
 	int dmgY = 0;
 
+	int roundsSurvived = 0;
+
 	public Rect graphWindow = new Rect (10, 60, 800, 400);
 
 	public GUIStyle shieldStyle = new GUIStyle();
@@ -33,6 +35,16 @@ public class FightState : MonoBehaviour
 		// get actual player here instead of creating new one
 		player = new Player ();
 		enemy = new Enemy ();
+		enemy.dice = 3;
+		enemy.sides = 5;
+		string log = "";
+		// test
+		/*foreach (int eventCount in RNG.getEventCount(2, 6))
+		{
+			log += eventCount + ", ";
+		}*/
+		Debug.Log(RNG.getMaximumAbsoluteProbability(enemy.dice, enemy.sides));
+		Debug.Log(log);
 	}
 	
 	// Update is called once per frame
@@ -49,21 +61,32 @@ public class FightState : MonoBehaviour
 		GUI.Label (new Rect(10, 40, 120, 20), "Water: " + (player.water - shield - pot));
 
 		GUI.Label (new Rect(140, 10, 120, 20), "Shields: " + shield);
-		shield = Mathf.RoundToInt (GUI.HorizontalSlider (new Rect(270, 10, 120, 20), shield, 0, maxDmg));
+		shield = Mathf.RoundToInt (GUI.HorizontalSlider (new Rect(270, 10, 120, 20), shield, 0, maxDmg - pot));
 
 		GUI.Label (new Rect(140, 40, 120, 20), "Bet: " + pot);
-		pot = Mathf.RoundToInt (GUI.HorizontalSlider (new Rect(270, 40, 120, 20), pot, 0, player.water - shield));
+		pot = Mathf.RoundToInt (GUI.HorizontalSlider (new Rect(270, 40, 120, 20), pot, 0, Mathf.Min(player.water - shield, maxDmg - shield)));
 
 		if (GUI.Button (new Rect(400, 10, 120, 50), "Hit me!"))
 		{
+			// reset pointer
+			dmgX = 0;
+			dmgY = 0;
 			StartCoroutine ("roll");
+			if (player.health > 0) {
+				roundsSurvived++;
+			}
 		}
+
+		GUI.Label (new Rect(530, 10, 150, 50), "Win: gain " + (pot + enemy.loot - shield) + " water");
+		GUI.Label (new Rect(530, 40, 150, 50), "Lose: gain " + (-pot - shield) + " water");
+
+		GUI.Label (new Rect(Screen.width - 160, 10, 150, 50), "You survived for " + roundsSurvived + " rounds");
 
 		// background (graph window)
 		GUI.Box (new Rect(graphWindow.x -10, graphWindow.y -10, graphWindow.width + 20, graphWindow.height +20), "");
 		// shield points
 		// number of possibilities for event
-		for (int y=0; y<=6; y++) 
+		for (int y=0; y<=RNG.getMaximumAbsoluteProbability(enemy.dice, enemy.sides); y++) 
 		{
 			// dmg
 			for (int x=0; x<=maxDmg; x++) 
@@ -75,7 +98,11 @@ public class FightState : MonoBehaviour
 					{
 						style = shieldStyle;
 					}
-					GUI.Button (new Rect(graphWindow.x + (x-1)*(graphWindow.width/maxDmg), graphWindow.y + y*(graphWindow.height/6), graphWindow.width/maxDmg, graphWindow.height/6), ""+(x), style);
+					if (dmgX == x && dmgY == y)
+					{
+						style = dmgStyle;
+					}
+					GUI.Label (new Rect(graphWindow.x + (x-1)*(graphWindow.width/maxDmg), graphWindow.y + y*(graphWindow.height/RNG.getMaximumAbsoluteProbability(enemy.dice, enemy.sides)), graphWindow.width/maxDmg, graphWindow.height/RNG.getMaximumAbsoluteProbability(enemy.dice, enemy.sides)), ""+(x), style);
 				}
 			}
 		}
@@ -90,15 +117,17 @@ public class FightState : MonoBehaviour
 		player.water -= shield;
 		player.water -= pot;
 
+		int[] eventCount = RNG.getEventCount(enemy.dice, enemy.sides);
 		// cycle
-		bool stopped = false; 
-		while (!stopped) {
-			if (dmgY < 6) {
+		int stopHeight = Random.Range(1, eventCount[dmg]+1);
+		while (true) {
+			if (dmgX == dmg && dmgY == stopHeight) 
+			{
+				break;
+			}
+			if (dmgY < eventCount[dmgX]) {
 				dmgY++;
-				if (dmgX = dmg) 
-				{
-
-				}
+				yield return new WaitForSeconds (0.009f);
 			}
 			else 
 			{
@@ -111,9 +140,9 @@ public class FightState : MonoBehaviour
 				{
 					dmgX = 0;
 				}
+				yield return new WaitForSeconds (0.009f);
 			}
 		}
-
 		// do damage if attack hits
 		if (dmg > shield) 
 		{
--- Assets/Scripts/RNG.cs
+++ Assets/Scripts/RNG.cs
@@ -9,16 +9,52 @@ public class RNG
 		int result = 0;
 		for (int i=0; i<dice; i++) 
 		{
-			result += Mathf.RoundToInt(Random.value * sides);
+			result += Mathf.RoundToInt(Random.value * (float)sides);
 		}
 		return result;
 	}
 
+	// i hope those words mean what i'm trying to say. way too long and stupid sounding anyway ...
+	public static int getMaximumAbsoluteProbability (int dice, int sides) 
+	{
+		int maximum = 0;
+		// again stupid approach ftw ...
+		foreach (int eventCount in getEventCount(dice, sides)) 
+		{
+			if (eventCount > maximum)
+			{
+				maximum = eventCount;
+			}
+		}
+		return maximum;
+	}
+
 	public static int[] getEventCount (int dice, int sides) 
 	{
-		//int[] eventCount = new int[dice*sides];
+		int[] eventCount = new int[dice*sides+1];
 		// ???? gaaaahrgh!!!!
-		int[] eventCount = {0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1};
+		// stupid approach: int[] eventCount = {0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1};
+		// slightly less stupid approach. Because it's a computer, and computers are proficient at stupidity, right?
+		// count ways to combine dice for all events. yes, all of them. ONE BY ONE.
+		eventCount = loop (sides, dice, eventCount, 0);
+		return eventCount;
+	}
+	private static int[] loop (int sides, int diceLeft, int[] eventCount, int sum)
+	{
+		for (int dx=1; dx<=sides; dx++) 
+		{
+			// open anoter loop
+			if (diceLeft > 1) 
+			{
+				loop (sides, diceLeft-1, eventCount, sum + dx);
+			}
+			// stop looping, start adding
+			else
+			{
+				int e = sum + dx;
+				eventCount[e]++;
+			}
+		}
 		return eventCount;
 	}
 }
