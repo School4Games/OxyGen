87ce095340b24781911e76d95596e6e0ed86d0f4
moe-ped <m.berning@web.de>
2014-12-04 18:18:58 +0100

    Brush test - added ability to paint tiles of different texture onto main texture

--- Assets/Scripts/WorldMap.cs
+++ Assets/Scripts/WorldMap.cs
@@ -12,115 +12,64 @@ public class tile {
 
 public class WorldMap : MonoBehaviour {
 
+	Texture2D main;
+
 	private tile[,] tiles;
 
 	public int width;
 	public int height;
 
-	public Material floors;
-	public int floorRows;
-	public Material structures;
-	public int structureRows;
-	public Material units;
-	public int unitRows;
+	public Texture2D[] layerTextures;
+		
+	public Texture2D stamp;
+	public int hexSideLength = 64;
+	
+	//Color[] stamp = new Color[100];
 
-	GameObject floorLayer;
-	GameObject structureLayer;
-	GameObject unitLayer;
+	void Start ()
+	{
+		// test
+		placeTile (new Vector2 (200, 200), 0);
+	}
+
+	void placeTile (Vector2 center, int texture) 
+	{
+		if (stamp != null) {
+			main = (Texture2D)renderer.material.mainTexture;
+
+			// calculate x and y coordinate from center and stamp dimensions
+			Vector2 startCorner = center - new Vector2(stamp.width/2, stamp.height/2);
+
+			// calculate brush colors from stamp grayscale (as alpha) and layer texture
+			Color[] brushColors = layerTextures[texture].GetPixels ((int)startCorner.x, (int)startCorner.y, stamp.width, stamp.height);
+			// get once and save?
+			Color[] stampColors = stamp.GetPixels ();
+			Color[] mapColors = main.GetPixels ((int)startCorner.x, (int)startCorner.y, stamp.width, stamp.height);
+			for (int i=0; i<brushColors.Length; i++)
+			{
+				brushColors[i] = ((brushColors[i] * stampColors[i].grayscale) + (mapColors[i] * (1-stampColors[i].grayscale)));
+			}
+
+			// calculate actual color to paint on map
+
+			// draw on texture
+			main.SetPixels((int)startCorner.x, (int)startCorner.y, stamp.width, stamp.height, brushColors);
+			main.Apply();
+		}
+	}
 
 	public void generate () {
 		tiles = new tile[width,height];
-		for (uint y=0; y<height; y++) {
-			for (uint x=0; x<width; x++) {
+		for (int y=0; y<height; y++) {
+			for (int x=0; x<width; x++) {
 				tiles[x,y] = new tile();
 			}
 		}
 		//do generation stuff
-		for (uint y=0; y<height; y++) {
-			for (uint x=0; x<height; x++) {
-
-			}
-		}
-		instantiateGraphics ();
-	}
-
-	public void instantiateGraphics () {
-		//create floor plane
-		//for testing
-		if (floorLayer != null) DestroyImmediate(floorLayer); 
-		if (floorLayer == null) floorLayer = createPlane(width, height, 1, floors);
-		//create structure plane
-		//create unit plane
-		//initiate 2D array
-	}
-
-	public GameObject createPlane (int xTiles, int yTiles, float tileSize, Material material) {
-		GameObject plane = new GameObject();
-		MeshFilter meshFilter = plane.AddComponent<MeshFilter>();
-		plane.AddComponent<MeshRenderer>();
-		plane.renderer.material = material;
-
-		meshFilter.sharedMesh = new Mesh();
-		Mesh mesh = meshFilter.sharedMesh;
-
-		Vector3[] vertices = new Vector3[xTiles * yTiles * 4];
-		int[] triangles = new int[xTiles * yTiles * 6];
-		Vector2[] uv = new Vector2[vertices.Length];
-
-		string log = "";
 		for (int y=0; y<height; y++) {
-			for (int x=0; x<width; x++) {
-				/*log += "x:" + x + "y:" + y + ", ";
-				vertices[0] = new Vector3(0, 0, 0);
-				vertices[1] = new Vector3(1, 0, 0);
-				vertices[2] = new Vector3(0, 1, 0);
-				vertices[3] = new Vector3(1, 1, 0);
+			for (int x=0; x<height; x++) {
 
-				log += (x+(y*width-1))*4 + ", " + (x+(y*width-1)+1)*4 + ", " + (x+(y*width-1)+2)*4 + ", " + (x+(y*width-1)+3)*4 + ", ";
-				
-				triangles[0] = 0;
-				triangles[1] = 2;
-				triangles[2] = 1;
-				triangles[3] = 1;
-				triangles[4] = 2;
-				triangles[5] = 3;*/
-				//vertices confirmed to work
-				vertices[x*4+y*width*4] = new Vector3(x, y, 0);
-				vertices[x*4+1+y*width*4] = new Vector3((x+1), y, 0);
-				vertices[x*4+2+y*width*4] = new Vector3(x, (y+1), 0);
-				vertices[x*4+3+y*width*4] = new Vector3((x+1), (y+1), 0);
-
-				if ((x*4+y*width*4) > 755) {
-				log += (x*4+y*width*4) + ":" + new Vector3(x, y, 0) + ", "
-					+ (x*4+1+y*width*4) + ":" + new Vector3((x+1), y, 0) + ", "
-					+ (x*4+2+y*width*4) + ":" + new Vector3(x, (y+1), 0) + ", "
-					+ (x*4+3+y*width*4) + ":" + new Vector3((x+1), (y+1), 0) + ", "
-					+ "\n";
-				}
-
-				triangles[x*6+y*width*6] = x*4+y*width;
-				triangles[x*6+1+y*width*6] = x*4+2+y*width;
-				triangles[x*6+2+y*width*6] = x*4+1+y*width;
-				triangles[x*6+3+y*width*6] = x*4+1+y*width;
-				triangles[x*6+4+y*width*6] = x*4+2+y*width;
-				triangles[x*6+5+y*width*6] = x*4+3+y*width;
-
-				uv[x*4+y*width] = new Vector2(0, 0);
-				uv[x*4+1+y*width] = new Vector2(1, 0);
-				uv[x*4+2+y*width] = new Vector2(0, 1);
-				uv[x*4+3+y*width] = new Vector2(1, 1);
 			}
 		}
-
-		Debug.Log(log);
-
-		mesh.Clear();
-		mesh.vertices = vertices;
-		mesh.triangles = triangles;
-		mesh.uv = uv;
-		mesh.RecalculateNormals();
-
-		plane.name = material.name;
-		return plane;
 	}
 }
