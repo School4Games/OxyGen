4070a2e238f6cd33cb2f5ee5a6ec6fd86cbe6660
moe-ped <m.berning@web.de>
2014-12-09 18:25:36 +0100

    MS3 version - added makeshift state switching

---
+++ Assets/Legacy/GameStateSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateSwitcher : MonoBehaviour {
+
+	// remove this script after MS3
+
+	public GameObject mapState;
+
+	public GameObject fightState;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			mapState.SetActive(!mapState.activeSelf);
+			fightState.SetActive(!fightState.activeSelf);
+		}
+	}
+}
