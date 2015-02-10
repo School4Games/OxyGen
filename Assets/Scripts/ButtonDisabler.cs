26651916de8db49ce2c0e8ac96160faad557be54
moe-ped <m.berning@web.de>
2015-02-10 17:56:47 +0100

    Button Fix - crafting buttons reset to interactable now once the requirements are met - changed some values for events

--- Assets/Scripts/ButtonDisabler.cs
+++ Assets/Scripts/ButtonDisabler.cs
@@ -4,17 +4,23 @@ using UnityEngine.UI;
 
 // what a horrible name
 // also: what a horrible way of doing this
-public class ButtonDisabler : MonoBehaviour {
+public class ButtonDisabler : MonoBehaviour 
+{
 
 	public int scrapCost;
 
 	public Player player;
 
 	// Update is called once per frame
-	void Update () {
-		if (player.inventory.resources[(int)Resource.Type.Scrap].amount < scrapCost)
+	void Update () 
+	{
+		if (player.inventory.resources [(int)Resource.Type.Scrap].amount < scrapCost) 
 		{
-			GetComponent<Button>().interactable = false;
+			GetComponent<Button> ().interactable = false;
+		} 
+		else 
+		{
+			GetComponent<Button> ().interactable = true;
 		}
 	}
 }
